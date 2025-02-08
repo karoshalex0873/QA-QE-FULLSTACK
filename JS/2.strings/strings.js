b095cf44f6b7bdecd55c0fcead3ff6ec87495d31
Alex Karobia <akarobia30@gmail.com>
2025-02-08 18:52:04 +0300

    assigment One done

--- JS/2.strings/strings.js
+++ JS/2.strings/strings.js
@@ -1,44 +1,3 @@
-// // string properties
-// // length
-// let studentName = 'Alice'
-// console.log(studentName.length)
-
-// // charAt -return specifed index in a string
-// console.log(studentName.charAt(3))
-
-// // concat method -joins two string
-// //   two ways 1. es5 or es6
-// // es5
-// let firstName = 'Julia'
-// let secondName = 'Karobia'
-// console.log(firstName.concat(' '+secondName))
-
-// // es6 -template strings
-// console.log(`${firstName} ${secondName}`)
-
-// // indexOf  retuns the firt index ina given string
-// // example
-// console.log(secondName.indexOf('a'))  // the a in karobia is in the 2nd index of the the array
-
-// // include property checks in the string contains a carint key
-// console.log(secondName.includes('a')) // returns [true]
-// // the second name contain  'a' hence results are true
-
-// // 6. toLowerCase and uppercase -makes a string to lower case
-// console.log(` lower_case: ${firstName.toLowerCase()}    upper_case  ${secondName.toUpperCase()} `)
-
-// // slipt  split an object(string) into arrays
-// console.log(firstName.split('')) //[ 'J', 'u', 'l', 'i', 'a' ]
-// // reversing and joining
-// console.log(firstName.split('').reverse().join(''))
-
-// // substing splict and return part of a string
-// console.log(secondName.substring(5,7)) // i and a are in the index of 5 and 7
-
-// // substr method
-// console.log(secondName.substr(4,3
-// ))
-
 // //   Asiigments of the Day
 // input is a string ?
 const is_string = (input) =>
@@ -117,7 +76,68 @@ console.log(swapcase("AaBbc"));
 
 // 11 Camelizing string
 const camelize = (string) => {
-  const camelizeString = string.split(" ");
+  const camelizeString = string.replace(/w+(.)/g, (match, chr) =>
+    ch.toUpperCase()
+  );
   return camelizeString;
 };
 console.log(camelize("JavaScript Exercises"));
+
+// uncamelizeing a string
+const uncamelize = (str, separator = "") =>
+  str.replace(/([a-z])([A-Z])/g, `$1${separator}$2`).toLowerCase();
+console.log(uncamelize("helloWorld"));
+
+// 13. Repeat String
+const repeat = (string, n) => string.repeat(n);
+console.log(repeat("Ha!", 3));
+// 14. nsert in String
+const insert = (string, insertString, pos) => {
+  const results = string.slice(0, pos) + insertString + ` ` + string.slice(pos);
+  return results;
+};
+console.log(insert("we are doing some exercies", "javaScript", 18));
+
+// 15. Humanize format
+const humanize_format = (num) => {
+  let suffix = ["th", "st", "nd", "rd"];
+  v = num % 100;
+  return num + (suffix[(v - 20) % 10] || suffix[v] || suffix[0]);
+};
+console.log(humanize_format(3));
+// 16. Trucate string with ellipsis
+
+const text_trancate = (string, length, ellipsis) =>
+  string.length > length ? string.slice(0, length) + ` ` + ellipsis : string;
+console.log(text_trancate("We are doing JS string", 15, "!!"));
+
+// 17. Chop String into Chunks
+const string_chop = (string, point) =>
+  string.match(new RegExp(`.{1,${point}}`, "g")) || [];
+console.log(string_chop("w3resource", 3));
+
+// 18. Count Substring Occurrence
+const count = (string, subString) => {
+  const stringNumber =
+    string.toLowerCase().split(subString.toLowerCase()).length - 1;
+  return stringNumber;
+};
+console.log(count("The quick brown fox jumped over the lazy dog", "the"));
+
+// 19. Revers Binary Respresenatation
+const reverse_binary = (num) => {
+  const BinaryForm = parseInt(num.toString(2).split("").reverse().join(""), 2);
+  return BinaryForm;
+};
+console.log(reverse_binary(100));
+
+// 20. Pad String to length
+const formatted_string = (pad, string, position) => {
+  string = string.toString();
+  return position === 'l' 
+    ? (pad + string).slice(-pad.length)  // Left padding
+    : (string + pad).slice(0, pad.length); // Right padding
+};
+
+console.log(formatted_string('0000', 123, 'l')); 
+
