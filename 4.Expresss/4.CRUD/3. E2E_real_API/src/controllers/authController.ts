1b2990bdd2d6a6cc78f99a6e8a8427858a981031
Alex Karobia <akarobia30@gmail.com>
2025-03-16 00:11:58 +0300

    authenutication backend with JWT token

--- 4.Expresss/4.CRUD/3. E2E_real_API/src/controllers/authController.ts
+++ 4.Expresss/4.CRUD/3. E2E_real_API/src/controllers/authController.ts
@@ -5,7 +5,7 @@ import pool from "../config/db.config"
 import bcrypt from 'bcryptjs';
 import { generateToken } from "../../utils/helpers/generateTokens";
 export const registerUser = asyncHandler(
-  async (req: Request, res: Response,next:NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const { name, email, password, role_id } = req.body
     //check if user is already registerd
     const userCheck = await pool.query("SELECT * FROM  users WHERE email = $1", [email])
@@ -23,10 +23,10 @@ export const registerUser = asyncHandler(
       "INSERT INTO users (name, email, password_hash, role_id) VALUES ($1, $2, $3, $4) RETURNING user_id, name, email, role_id",
       [name, email, hashedPassword, role_id]
     );
-    
+
 
     //Generate JWT token
-    generateToken(res,newUser.rows[0].id, newUser.rows[0].role_id)
+    generateToken(res, newUser.rows[0].id, newUser.rows[0].role_id)
 
     res.status(201).json({ message: "User registered successfully", user: newUser.rows[0] })
 
@@ -34,3 +34,46 @@ export const registerUser = asyncHandler(
 
   }
 )
+
+
+export const loginUser = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    
+
+    // Check if user exists
+    const userCheck = await pool.query(`
+      SELECT users.user_id, users.name, users.email, users.password_hash, users.role_id, user_role.role_name 
+      FROM users 
+      JOIN user_role ON users.role_id = user_role.role_id 
+      WHERE users.email = $1
+    `, [email]);
+
+    if (userCheck.rows.length === 0) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const user = userCheck.rows[0];
+
+    // Compare hashed password
+    const isPasswordMatch = await bcrypt.compare(password, user.password_hash);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // Generate JWT token
+    generateToken(res, user.user_id, user.role_id);
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        id: user.user_id,
+        name: user.name,
+        email: user.email,
+        role: user.role_name
+      }
+    });
+  }
+);
