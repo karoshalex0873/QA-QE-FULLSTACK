f352d149a3d231f5426603bf0510de8ab256fecf
Alex Karobia <akarobia30@gmail.com>
2025-03-24 00:04:30 +0300

    all 4 types of data bindig in Angular

--- 5.Angular/1.intro/helloAngular/src/app/app.component.ts
+++ 5.Angular/1.intro/helloAngular/src/app/app.component.ts
@@ -1,27 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+//string interpolation
+title='Angular Data Binding'
+//property binding
+isDisabled = true;
+imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+
   //details of a person
-  person: {
-    name: string;
-    age: number;
-    city: string;
-  } = {
-      name: 'Alex Smith',
-      age: 30,
-      city: 'New York'
-    };
-//printing them here
-getInfo = () => {
-  return `${this.person.name} lives in ${this.person.city} years old ${this.person.age}`;
-  }
+person ={name:'Alex karobia'}
+message: string = ''; 
 
-
+sayHello = () => {
+  this.message = `Hello world! this is my first Angular app! my name is ${this.person.name.split(' ')[0]}`;
+}
+//twoway data binding
+username: string = ''
 }
