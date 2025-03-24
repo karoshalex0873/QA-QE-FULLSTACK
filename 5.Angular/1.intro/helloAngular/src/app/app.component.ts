75949f816784c32bc9ac8cd792cc1e7e36d12134
Alex Karobia <akarobia30@gmail.com>
2025-03-24 08:56:12 +0300

    Directives

--- 5.Angular/1.intro/helloAngular/src/app/app.component.ts
+++ 5.Angular/1.intro/helloAngular/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule],
+  imports: [CommonModule,RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -25,4 +26,28 @@ sayHello = () => {
 }
 //twoway data binding
 username: string = ''
+
+
+
+// Lesson 2 diresctives
+// condition for *ngIF
+isloggedIn = false;
+
+// looping over items using ngFor
+cars = ['Toyota','Honda','BMW','Mercedes','Nissan'];
+// object to loop over using ngFor
+carObject:{name:string,model:string,year:number}= {
+  name: 'Toyota',
+  model: 'Corolla',
+  year: 2022
+};
+
+// ngSwitch 
+userRole = 'user'; // can be 'admin', 'user', or 'guest'
+
+// 2 Attributes Directives
+// ngClass  - Dynamically add or remove  CSS classes
+isHighlighted = false;
+// ngStyle - Dynamically set CSS styles
+color='blue';
 }
