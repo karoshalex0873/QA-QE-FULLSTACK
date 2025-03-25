d7118d9ceb6e488929d50db0906aac7b2d941845
Alex Karobia <akarobia30@gmail.com>
2025-03-25 23:30:56 +0300

    forms

--- 5.Angular/1.intro/helloAngular/src/app/about/about.component.ts
+++ 5.Angular/1.intro/helloAngular/src/app/about/about.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FormsModule,NgClass],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  onsubmit(form:any){
+    console.log(form)
+  }
 }
