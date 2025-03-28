0be86159bb3e9337c65420f1daebc1f3face4d98
Alex Karobia <akarobia30@gmail.com>
2025-03-28 09:25:38 +0300

    array froms in angular

--- 5.Angular/1.intro/helloAngular/src/app/array-forms/array-forms.component.ts
+++ 5.Angular/1.intro/helloAngular/src/app/array-forms/array-forms.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-array-forms',
-  imports: [],
+  standalone: true, // For standalone components
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './array-forms.component.html',
-  styleUrl: './array-forms.component.css'
+  styleUrls: ['./array-forms.component.css'] // Fix the typo from `styleUrl` to `styleUrls`
 })
-export class ArrayFormsComponent {
+export class ArrayFormsComponent { 
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      skills: this.fb.array([]) // Name this 'skills' to match the template
+    });
+  }
+
+  get skills(): FormArray {
+    return this.form.get('skills') as FormArray;
+  }
+
+  addSkill(): void {
+    this.skills.push(this.fb.control('')); // Add a new control to the FormArray
+  }
+
+  removeSkill(index: number): void {
+    this.skills.removeAt(index); // Remove the control at the specified index
+  }
 }
